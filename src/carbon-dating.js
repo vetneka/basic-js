881ae71314c364daef903adb76800f02a5237715
vetneka <vetneka@yandex.ru>
2021-02-27 21:57:51 +0500

    feat: add handle floating-point numbers

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  const sampleActivityNormalized = parseInt(sampleActivity);
+  const sampleActivityNormalized = parseFloat(sampleActivity);
 
   if (
     typeof sampleActivity !== 'string'||
